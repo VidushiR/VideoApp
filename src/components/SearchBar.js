d65a8de3535bbc93374c8bfd3fa6af39c4ddfcf8
VidushiR <vidsraval@gmail.com>
2019-08-06 12:40:19 -0700

    first commit to video application

---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
+  render() {
+    return (
+      <div className="search-bar">
+        <i className="glyphicon glyphicon-search" />
+
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+      </div>
+    );
+  }
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
+}
+export default SearchBar;
